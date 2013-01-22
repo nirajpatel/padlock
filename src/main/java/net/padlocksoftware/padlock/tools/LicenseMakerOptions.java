7c2ff266ad47561d112ddcf5ae0a689fb76958db
Jason Pell <jason@pellcorp.com>
2013-01-22 17:31:06 +1100

    further refactoring to start using the options class.

--- src/main/java/net/padlocksoftware/padlock/tools/LicenseMakerOptions.java
+++ src/main/java/net/padlocksoftware/padlock/tools/LicenseMakerOptions.java
@@ -2,15 +2,15 @@ package net.padlocksoftware.padlock.tools;
 
 import java.io.File;
 import java.io.StringWriter;
-import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
-import org.kohsuke.args4j.spi.StringArrayOptionHandler;
 
 public class LicenseMakerOptions {
     private final CmdLineParser parser;
@@ -18,8 +18,8 @@ public class LicenseMakerOptions {
     @Option(name="-k", metaVar="Key File", usage="Key File", required=true)
     private File keyFile;
     
-    @Option(name="-o", metaVar="Output File", usage="Output License File")
-    private File outputFile;
+    @Option(name="-o", metaVar="License File", usage="Output License File")
+    private File licenseFile;
     
     @Option(name="-O", usage="end output to standard out instead of a file")
     private boolean standardOut;
@@ -60,8 +60,8 @@ public class LicenseMakerOptions {
         parser = new CmdLineParser(this);
         parser.parseArgument(args);
         
-        if (outputFile == null && !standardOut) {
-            throw new CmdLineException(parser, "OutputFile or Standard Out flag required");
+        if (licenseFile == null && !standardOut) {
+            throw new CmdLineException(parser, "License File or Standard Out flag required");
         }
         
         if (startInMs != null && startDate != null) {
@@ -70,6 +70,22 @@ public class LicenseMakerOptions {
         
         if (expirationInMs != null && expirationDate != null) {
             throw new CmdLineException(parser, "Only one of ExpirationDate or ExpirationInMs allowed");
+        }
+        
+        if (startInMs != null) {
+            startDate = new Date(startInMs);
+        }
+        
+        if (expirationInMs != null) {
+            expirationDate = new Date(expirationInMs);
+        }
+        
+        if (properties == null) {
+            properties = new HashMap<String, String>();
+        }
+        
+        if (addresses == null) {
+            addresses = new HashSet<String>();
         }
     }
     
@@ -83,24 +99,16 @@ public class LicenseMakerOptions {
         return keyFile;
     }
 
-    public File getOutputFile() {
-        return outputFile;
+    public File getLicenseFile() {
+        return licenseFile;
     }
 
     public boolean isStandardOut() {
         return standardOut;
     }
 
-    public Long getStartInMs() {
-        return startInMs;
-    }
-
     public Date getStartDate() {
         return startDate;
-    }
-
-    public Long getExpirationInMs() {
-        return expirationInMs;
     }
 
     public Date getExpirationDate() {
