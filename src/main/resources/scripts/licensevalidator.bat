50752b3fe2032d821aad46643cea9cfda56f1c31
Jason Pell <jason@pellcorp.com>
2014-05-30 11:20:24 +1000

    fixed up scripts and added batch files

---
+++ src/main/resources/scripts/licensevalidator.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+set EXE_DIR=%~dp0
+FOR /R %EXE_DIR%\lib %%G IN (*.jar) DO set CLASSPATH=!CLASSPATH!;%%G
+
+java net.padlocksoftware.padlock.tools.LicenseValidator %*
+
--- src/main/resources/scripts/licensevalidator.sh
+++ src/main/resources/scripts/licensevalidator.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-java -cp "lib/*" net.padlocksoftware.padlock.tools.LicenseValidator $@
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -cp "$DIR/lib/*" net.padlocksoftware.padlock.tools.LicenseValidator $@
 
